29892481920eb0d8fdd8864388e748d5f1160b7d
mardg09 <153201150+mardg09@users.noreply.github.com>
2024-12-01 23:50:49 +0800

    Just fixed the format of the code

--- Marketing Department/ET_Marketing.py
+++ Marketing Department/ET_Marketing.py
@@ -5,20 +5,10 @@ import re
 csv_path = "Raw Data/campaign_data.csv"
 df1 = pd.read_csv(csv_path, delimiter="\t")
 
-# Remove unnecessary quotations
-df1['campaign_description'] = df1['campaign_description'].str.replace('"', '', regex=False)
-
 # Delete unnecessary columns
 unnamed_columns = [col for col in df1.columns if col.lower().startswith('unnamed')]
 if unnamed_columns:
     df1.drop(columns=unnamed_columns, inplace=True)
-
-# Standardize campaign_id
-df1['campaign_id'] = df1['campaign_id'].str.replace('CAMPAIGN', 'C')
-
-# Standardize campaign_name
-df1['campaign_name'] = df1['campaign_name'].str.capitalize()
-df1['campaign_name'] = df1['campaign_name'].str.replace(r'\bi\b', 'I', regex=True)
 
 # Check for duplicate campaign_name and remove duplicates
 duplicate_campaign_names = df1[df1.duplicated(subset=['campaign_name'], keep=False)]
@@ -37,6 +27,16 @@ if null_campaign_data.any():
     print(null_campaign_data)
 else:
     print("No null values in campaign data.")
+
+# Remove unnecessary quotations
+df1['campaign_description'] = df1['campaign_description'].str.replace('"', '', regex=False)
+
+# Standardize campaign_id
+df1['campaign_id'] = df1['campaign_id'].str.replace('CAMPAIGN', 'C')
+
+# Standardize campaign_name
+df1['campaign_name'] = df1['campaign_name'].str.capitalize()
+df1['campaign_name'] = df1['campaign_name'].str.replace(r'\bi\b', 'I', regex=True)
 
 # Convert percentage
 def standardize_percentage(value):
@@ -60,17 +60,6 @@ unnamed_columns = [col for col in df2.columns if col.lower().startswith('unnamed
 if unnamed_columns:
     df2.drop(columns=unnamed_columns, inplace=True)
 
-# Standardize campaign_id
-df2['campaign_id'] = df2['campaign_id'].str.replace('CAMPAIGN', 'C')
-
-# Standardize transaction_date
-df2['transaction_date'] = pd.to_datetime(df2['transaction_date'], errors='coerce').dt.strftime('%Y-%m-%d')
-
-# Standardize estimated arrival
-df2.rename(columns={'estimated arrival': 'estimated_arrival_in_days'}, inplace=True)
-df2['estimated_arrival_in_days'] = df2['estimated_arrival_in_days'].apply(lambda x: re.sub(r'\D', '', str(x)))
-df2['estimated_arrival_in_days'] = pd.to_numeric(df2['estimated_arrival_in_days'], errors='coerce')
-
 # Check for duplicate order_ids
 duplicate_order_ids = df2[df2.duplicated(subset=['order_id'], keep=False)]
 if not duplicate_order_ids.empty:
@@ -88,6 +77,17 @@ if null_transactional_data.any():
     print(null_transactional_data)
 else:
     print("No null values in transactional campaign data.")
+
+# Standardize campaign_id
+df2['campaign_id'] = df2['campaign_id'].str.replace('CAMPAIGN', 'C')
+
+# Standardize transaction_date
+df2['transaction_date'] = pd.to_datetime(df2['transaction_date'], errors='coerce').dt.strftime('%Y-%m-%d')
+
+# Standardize estimated arrival
+df2.rename(columns={'estimated arrival': 'estimated_arrival_in_days'}, inplace=True)
+df2['estimated_arrival_in_days'] = df2['estimated_arrival_in_days'].apply(lambda x: re.sub(r'\D', '', str(x)))
+df2['estimated_arrival_in_days'] = pd.to_numeric(df2['estimated_arrival_in_days'], errors='coerce')
 
 # Save the cleaned DataFrame
 df2.to_csv("Cleaned Data/cleaned_transactional_campaign_data.csv", index=False)
